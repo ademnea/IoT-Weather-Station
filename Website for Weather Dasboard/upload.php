9a59e7479fae27ec1ffe22585d069cae5ec3ee18
Muhairwe Arnold <49070870+MuhairweArnold@users.noreply.github.com>
2022-11-04 18:19:05 +0300

    Update upload.php

--- Website for Weather Dasboard/upload.php
+++ Website for Weather Dasboard/upload.php
@@ -1,18 +1,1 @@
-<?php
-include_once'database.php';
-$humidity = $_GET['humidity'];
-$temperature = $_GET['temperature'];
-$rainfall = $_GET['rainfall'];
-$wind_speed = $_GET['wind_speed'];
-$wind_direction = $_GET['wind_direction'];
-$dailyrainfall = $_GET['dailyrainfall'];
-$pressure = $_GET['pressure'];
 
-$result = insertReading($temperature, $humidity, $pressure, $rainfall, $wind_speed, $wind_direction, $dailyrainfall);
-echo $result;
-
-$data = trim($data);
-$data = stripslashes($data);
-$data = htmlspecialchars($data);
-return $data;
-?>
